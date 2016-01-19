53c5c049b406e35ca79211256c2e7a45a27c33c4
Luis Gallet <nanogallet@gmail.com>
2016-01-18 23:26:47 -0500

    finished part 4 but not tested

---
+++ D_FF.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity D_FF is
+   port
+   (
+      clk : in std_logic;
+
+		enable : in std_logic := '1';
+      
+      d : in std_logic;
+
+      q : out std_logic
+   );
+
+end D_FF;
+ 
+architecture behavioral of D_FF is
+begin
+   process (clk) is
+   begin
+      if rising_edge(clk) then  
+         if (enable='1') then   
+            q <= d;
+         end if;
+      end if;
+   end process;
+end behavioral;
